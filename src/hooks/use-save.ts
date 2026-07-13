0b34b20116e3ee46747181649359738fed1011c8
Anirban-1490 <anirbanpratihar45@gmail.com>
2026-07-13 21:29:22 +0530

    feat: added a hook to save a property

---
+++ src/hooks/use-save.ts
@@ -0,0 +1,97 @@
+import { useAuth } from "@clerk/expo";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useAuthSupabase } from "./use-supabase";
+
+export const useSaveProperty = (propertyId: string, onSave?: () => void) => {
+  const { userId } = useAuth();
+  const supabase = useAuthSupabase();
+  const queryClient = useQueryClient();
+
+  const {
+    data: isPropertySaved,
+    isLoading,
+    isError: propertySaveFetchError,
+  } = useQuery({
+    queryKey: ["fetch-saved-property", propertyId],
+    queryFn: async () => {
+      return await supabase
+        .from("saved_properties")
+        .select("id")
+        .eq("user_clerk_id", userId!)
+        .eq("property_id", propertyId)
+        .single();
+    },
+    enabled: !!userId,
+  });
+
+  const {
+    mutate: saveProperty,
+    isPending: isSavingProperty,
+    data: savePropertyData,
+    isError: isSavingPropertyError,
+  } = useMutation({
+    mutationKey: ["save-property", propertyId],
+    mutationFn: async () => {
+      if (!userId) return;
+      return await supabase.from("saved_properties").insert({
+        user_clerk_id: userId,
+        property_id: propertyId,
+      });
+    },
+    onSettled: async (data, error) => {
+      if (data?.error || error) return;
+
+      await queryClient.invalidateQueries({
+        queryKey: ["fetch-saved-property", propertyId],
+      });
+    },
+  });
+  const {
+    mutate: removeSavedProperty,
+    isPending: isRemovingSavedProperty,
+    data,
+    isError,
+  } = useMutation({
+    mutationKey: ["remove-save-property", propertyId],
+    mutationFn: async () => {
+      if (!userId) return;
+      return await supabase
+        .from("saved_properties")
+        .delete()
+        .eq("user_clerk_id", userId!)
+        .eq("property_id", propertyId);
+    },
+    onSettled: async (data, error) => {
+      if (data?.error || error) return;
+
+      await queryClient.invalidateQueries({
+        queryKey: ["fetch-saved-property", propertyId],
+      });
+    },
+  });
+
+  const toggleSave = () => {
+    if (!userId) return;
+
+    if (isPropertySaved?.data) {
+      removeSavedProperty();
+    } else {
+      saveProperty();
+    }
+  };
+
+  const saveLoading = isSavingProperty || isRemovingSavedProperty;
+  const saveError =
+    !!data?.error ||
+    isError ||
+    isSavingPropertyError ||
+    !!savePropertyData?.error;
+
+  return {
+    isPropertySaved: !!isPropertySaved?.data,
+    propertySaveFetchError,
+    saveLoading,
+    toggleSave,
+    saveError,
+  };
+};
